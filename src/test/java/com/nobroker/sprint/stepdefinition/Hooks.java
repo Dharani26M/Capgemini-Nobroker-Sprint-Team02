f2f5cdeb9b678e36fa06a564d91164b71029cf7a
Dharani26M <itsdharani26@gmail.com>
2026-04-17 19:19:42 +0530

    Resolved merge conflict in Hooks and added BuyTest

--- src/test/java/com/nobroker/sprint/stepdefinition/Hooks.java
+++ src/test/java/com/nobroker/sprint/stepdefinition/Hooks.java
@@ -1,10 +1,9 @@
-package com.nobroker.sprint.stepdefinition;
-import java.io.IOException;
+package com.nobroker.sprint.stepDefinition;
 
+import java.io.IOException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
 import com.nobroker.sprint.utils.AllUtilities;
 import com.nobroker.sprint.utils.BaseClass;
 import com.nobroker.sprint.utils.Pages;
@@ -13,50 +12,42 @@ import io.cucumber.java.Before;
 
 public class Hooks extends AllUtilities {
 
-	// call the driver base using dependency injection -DI
-		private BaseClass bhook;
+    private BaseClass bhook;
 
-		public Hooks(BaseClass b) {
-			this.bhook = b;
-		}
+    public Hooks(BaseClass b) {
+        this.bhook = b;
+    }
 
-		// DI part
+    @Before
+    public void setup() throws IOException {
+        String Url = getPropertyKeyValue("Url");
+        String phone = getPropertyKeyValue("PhoneNo");
+        String Browser = getPropertyKeyValue("browser");
 
-		// setup precondition
-		@Before
-		public void setup() throws IOException {
+        if (Browser.equalsIgnoreCase("Edge"))
+            bhook.driver = new EdgeDriver();
+        else if (Browser.equalsIgnoreCase("chrome"))
+            bhook.driver = new ChromeDriver();
+        else
+            bhook.driver = new FirefoxDriver();
+        
+        initializeDriver(bhook.driver);
+        ConfigMaximizeBrowser();
+        
+        // I kept your 80 second wait to ensure stability
+        WaitForAllElements(80); 
+        EnterUrl(Url);
 
-			String Url = getPropertyKeyValue("Url");
-			String phone = getPropertyKeyValue("PhoneNo");
-			String Browser=getPropertyKeyValue("browser");
+        Pages.LoadAllPages(bhook.driver);
 
-			// lanuch the browser
+        // Kept the Login logic from your local changes
+        Pages.dashpage.LoginIn(bhook.driver, phone);
+    }
 
-			if(Browser.equalsIgnoreCase("Edge"))
-				bhook.driver = new EdgeDriver();
-			else if(Browser.equalsIgnoreCase("chrome"))
-				bhook.driver= new ChromeDriver();
-			else
-				bhook.driver=new FirefoxDriver();
-			
-			initializeDriver(bhook.driver);
-			
-			ConfigMaximizeBrowser();
-			WaitForAllElements(80);
-			EnterUrl(Url);
-
-			// initialize the pages
-			Pages.LoadAllPages(bhook.driver);
-
-			// login
-			Pages.dashpage.LoginIn(bhook.driver, phone);
-		}
-
-		// post condition
-		@After
-		public void teardown() {
-			if (bhook.driver != null) {
-//				QuitBrowser();
-			}
-		}
+    @After
+    public void teardown() {
+        if (bhook.driver != null) {
+            // QuitBrowser(); 
+        }
+    }
 }
